b8003b9e9f6aeeb7f29b3bd21822f7e41c0a29fe
Boris B <bamboriz1@gmail.com>
2023-10-19 19:33:27 +0300

    aider: Refactor app.py to use a new function get_response(query) from response_handler.py.
    # Aider chat conversation:
    
    ASSISTANT: To simplify the `app.py` file, we can create a new function `get_response(query)` in a new file `response_handler.py`. This function will contain the logic currently in the `if st.button("Submit Query", type="primary"):` block. The `app.py` file will then simply call this function when the button is clicked, get the response, and write it out.
    
    Here are the changes:
    
    1. Create a new file `response_handler.py`:
    
    ```python
    response_handler.py
    <<<<<<< HEAD
    =======
    import streamlit as st
    from langchain.chat_models import ChatOpenAI
    from langchain.agents import initialize_agent
    from langchain.agents import AgentType
    from retrieval_tool import info_retrieval_tool
    
    def get_response(query):
        tools = [info_retrieval_tool()]
        llm = ChatOpenAI(temperature=0, model_name='gpt-4')
        agent = initialize_agent(llm=llm,
                tools=tools,
                agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
                verbose=True)
        prompt = f'''
            You are a helpful medical assistant! You only respond to medical queries and say 'I don't know' for anything else!
            Let's decode the way to respond to the queries.
            For all medical queries in addition to providing a relevant answer, also add the details (name, availability, location and contact info ...) of 1 or more relevant Medical personnel.
            When no specialist matches, recommend a General Practitioner.
            Think step by step
            {query}
            '''
        response = agent.run(prompt)
        return response
    >>>>>>> updated
    ```
    
    2. Modify `app.py` to use the new `get_response(query)` function:
    
    ```python
    app.py
    <<<<<<< HEAD
    if st.button("Submit Query", type="primary"):
        tools = [info_retrieval_tool()]
        llm = ChatOpenAI(temperature=0, model_name='gpt-4')
        agent = initialize_agent(llm=llm,
                tools=tools,
                agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
                verbose=True)
        prompt = f'''
            You are a helpful medical assistant! You only respond to medical queries and say 'I don't know' for anything else!
            Let's decode the way to respond to the queries.
            For all medical queries in addition to providing a relevant answer, also add the details (name, availability, location and contact info ...) of 1 or more relevant Medical personnel.
            When no specialist matches, recommend a General Practitioner.
            Think step by step
            {query}
            '''
        response = agent.run(prompt)
        st.write(response)
    =======
    from response_handler import get_response
    
    if st.button("Submit Query", type="primary"):
        response = get_response(query)
        st.write(response)
    >>>>>>> updated
    ```
    
    Please add `response_handler.py` to the chat so I can propose changes to it.

--- app.py
+++ app.py
@@ -11,21 +11,9 @@ st.title("👨‍💻 Chat with Medico")
 
 query = st.text_area("Send a Message")
 
+from response_handler import get_response
+
 if st.button("Submit Query", type="primary"):
-    tools = [info_retrieval_tool()]
-    llm = ChatOpenAI(temperature=0, model_name='gpt-4')
-    agent = initialize_agent(llm=llm,
-            tools=tools,
-            agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-            verbose=True)
-    prompt = f'''
-        You are a helpful medical assistant! You only respond to medical queries and say 'I don't know' for anything else!
-        Let's decode the way to respond to the queries.
-        For all medical queries in addition to providing a relevant answer, also add the details (name, availability, location and contact info ...) of 1 or more relevant Medical personnel.
-        When no specialist matches, recommend a General Practitioner.
-        Think step by step
-        {query}
-        '''
-    response = agent.run(prompt)
+    response = get_response(query)
     st.write(response)
 
