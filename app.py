41bb33d7e61a22eecf594cee6f7557075a51040d
Boris B <bamboriz1@gmail.com>
2023-10-19 19:40:28 +0300

    aider: Added error handling to the `get_response` function.
    # Aider chat conversation:
    
    USER: add error handling to get_response
    
    ASSISTANT: To add error handling to the `get_response` function in the `app.py` file, we will use a try-except block. This will allow us to catch any exceptions that occur when the function is called and handle them appropriately.
    
    Here is the *edit block* for this change:
    
    ```python
    app.py
    <<<<<<< HEAD
    def get_response(query):
        tools = [info_retrieval_tool()]
        llm = ChatOpenAI(temperature=0, model_name='gpt-4')
        agent = initialize_agent(llm=llm,
                tools=tools,
                agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
                verbose=True)
        with open('prompts/medical_query_prompt.txt', 'r') as file:
            prompt_template = file.read()
        prompt = prompt_template.format(query=query)
        response = agent.run(prompt)
        return response
    =======
    def get_response(query):
        try:
            tools = [info_retrieval_tool()]
            llm = ChatOpenAI(temperature=0, model_name='gpt-4')
            agent = initialize_agent(llm=llm,
                    tools=tools,
                    agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
                    verbose=True)
            with open('prompts/medical_query_prompt.txt', 'r') as file:
                prompt_template = file.read()
            prompt = prompt_template.format(query=query)
            response = agent.run(prompt)
            return response
        except Exception as e:
            return f"An error occurred: {str(e)}"
    >>>>>>> updated
    ```
    
    This change will catch any exceptions that occur when the `get_response` function is called and return a string with the error message. This will prevent the application from crashing if an error occurs.

--- app.py
+++ app.py
@@ -6,17 +6,20 @@ from config import *
 from retrieval_tool import info_retrieval_tool
 
 def get_response(query):
-    tools = [info_retrieval_tool()]
-    llm = ChatOpenAI(temperature=0, model_name='gpt-4')
-    agent = initialize_agent(llm=llm,
-            tools=tools,
-            agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-            verbose=True)
-    with open('prompts/medical_query_prompt.txt', 'r') as file:
-        prompt_template = file.read()
-    prompt = prompt_template.format(query=query)
-    response = agent.run(prompt)
-    return response
+    try:
+        tools = [info_retrieval_tool()]
+        llm = ChatOpenAI(temperature=0, model_name='gpt-4')
+        agent = initialize_agent(llm=llm,
+                tools=tools,
+                agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+                verbose=True)
+        with open('prompts/medical_query_prompt.txt', 'r') as file:
+            prompt_template = file.read()
+        prompt = prompt_template.format(query=query)
+        response = agent.run(prompt)
+        return response
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
 st.set_page_config(page_title="👨‍💻 Chat with Medico")
 
